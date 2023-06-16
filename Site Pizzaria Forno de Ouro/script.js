bb68be5f4dab9e17d231fb73ae52555e1ddf2a0c
Matheus Inácio De Almeida Arruda <81710120+Matheus-Inacioal@users.noreply.github.com>
2023-06-16 19:55:00 -0300

    Delete Site Pizzaria Forno de Ouro directory

--- Site Pizzaria Forno de Ouro/script.js
+++
@@ -1,8 +0,0 @@
-// Implementar funcionalidade do botão "Adicionar ao carrinho"
-var addButtons = document.querySelectorAll('.menu-item button');
-
-for (var i = 0; i < addButtons.length; i++) {
-addButtons[i].addEventListener('click', function() {
-alert('Pizza adicionada ao carrinho!');
-});
-}
